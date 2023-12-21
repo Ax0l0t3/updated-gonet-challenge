755218a80ee4fb361cd8dcfc4a9285b6e56796b3
D4v3 <antenopolis@gmail.com>
2023-12-21 15:05:04 -0600

    More prettier changes

--- src/pages/LiComponent.js
+++ src/pages/LiComponent.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import styles from '../styles/checkbox-style.module.css';
+import React from 'react'
+import styles from '../styles/checkbox-style.module.css'
 
 export const LiComponent = () => {
   return (
-  <li>
-    <div className="border-b-2 flex items-center">
-      <p className="mx-2 mb-1">Image</p>
-      <p>Title</p>
-      <label className={styles.container}>
-        <input id="1234567890" className={styles.thisInput} type="checkbox" />
-        <div className={styles.forCheckbox} />
-      </label>
-    </div>
-  </li>
+    <li>
+      <div className="border-b-2 flex items-center">
+        <p className="mx-2 mb-1">Image</p>
+        <p>Title</p>
+        <label className={styles.container}>
+          <input id="1234567890" className={styles.thisInput} type="checkbox" />
+          <div className={styles.forCheckbox} />
+        </label>
+      </div>
+    </li>
   )
 }
