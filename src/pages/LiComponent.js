a37ab19e6f92f875c48234129258c7d0a6c5760a
D4v3 <antenopolis@gmail.com>
2023-12-27 21:59:39 -0600

    Added series pictures; Filter series

--- src/pages/LiComponent.js
+++ src/pages/LiComponent.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "../styles/checkbox-style.module.css";
 
-export const LiComponent = () => {
+export const LiComponent = ({movieName, moviePicture}) => {
   return (
     <li>
       <div className="border-b-2 flex items-center">
-        <p className="mx-2 mb-1">Image</p>
-        <p>Title</p>
+        <img className="mx-2 mb-1" src={moviePicture} />
+        <p>{movieName}</p>
         <label className={styles.container}>
-          <input id="1234567890" className={styles.thisInput} type="checkbox" />
+          <input className={styles.thisInput} type="checkbox" />
           <div className={styles.forCheckbox} />
         </label>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,27 @@ import { LiComponent } from "./LiComponent";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
+  const [movieArray, setMovieArray] = useState([]);
+  const [sortedArray, setSortedArray] = useState([]);
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
+    const newArray = movieArray.filter(movie => movie.name.toLowerCase().includes(e.target.value.toLowerCase()));
+    setSortedArray(newArray);
   };
 
   async function logMovies() {
-    const response = await fetch("https://api.tvmaze.com/search/shows?q=all");
+    const response = await fetch("https://api.tvmaze.com/shows");
     const jsonResponse = await response.json();
+    jsonResponse.sort((firstMovie, secondMovie) =>{
+      const lowerCaseName = firstMovie.name.toLowerCase();
+      const lowerCaseComparison = secondMovie.name.toLowerCase();
+      if(lowerCaseName < lowerCaseComparison) return -1;
+      if(lowerCaseName > lowerCaseComparison) return 1;
+      return 0;
+    })
+    setMovieArray(jsonResponse);
+    setSortedArray(jsonResponse);
   }
 
   useEffect(() => {
@@ -20,7 +33,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col h-screen justify-center items-center">
+    <main className="flex flex-col justify-center items-center">
       <h1 className="text-5xl">My TV Shows</h1>
       <input
         className="my-4 color text-[#471131]"
@@ -33,10 +46,9 @@ export default function Home() {
         View Favourites
       </button>
       <ul className="w-full h-full flex flex-col justify-between">
-        <LiComponent />
-        <LiComponent />
-        <LiComponent />
-        <LiComponent />
+        {sortedArray.map((movie, index) => {
+          return <LiComponent key={index} movieName={movie.name} moviePicture={movie.image.medium}/>
+        })}
       </ul>
     </main>
   );
